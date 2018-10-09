ad3b6d1629edeff2e35f5fa62b8f927d90f2bd07
t00199360 <t00199360@students.ittralee.ie>
2018-10-09 13:49:04 +0100

    Bank account final part is v hard to do lol

--- src/BankAccountDriver.java
+++ src/BankAccountDriver.java
@@ -4,27 +4,34 @@ public class BankAccountDriver
 {
     public static void main(String[] args)
     {
+       whole();
 
-        String balanceAsString=JOptionPane.showInputDialog("Please enter your balance: ");
-        double balance=Double.parseDouble(balanceAsString);
+    }
+    public static String whole()
+    {
+
+        String balanceAsString = JOptionPane.showInputDialog("Please enter your balance: ");
+        double balance = Double.parseDouble(balanceAsString);
 
         BankAccount account1 = new BankAccount();
-        BankAccount account2 = new BankAccount();
 
         BankAccountName accountCustomer = new BankAccountName();
         accountCustomer.setName(JOptionPane.showInputDialog("Please enter your name: "));
-
 
 
         account1.setAccnum("3245");
         account1.setBalance(balance);
         account1.setCustomer(accountCustomer);
 
-        JOptionPane.showMessageDialog(null,"" + account1.toString());
+        String bankAccounts [] = new String[]{account1.toString()};
+        JOptionPane.showMessageDialog(null, "" + account1.toString());
 
         BankAccount.lodgement(balance);
         System.exit(0);
+        return account1.toString();
     }
+
+
 
 
 }
---
+++ src/BankAccountDriver_v2.java
@@ -0,0 +1,18 @@
+public class BankAccountDriver_v2
+{
+    public static void main(String[] args)
+    {
+        BankAccountDriver.whole();
+
+        String [] arrayAccount = new String []
+                {
+                     (arrayAccount);
+                }
+
+
+    }
+    public String Function()
+    {
+
+    }
+}
